b0ee1328f7df3c1a61a73c7bc2b172216e2a22fe
beomseok.choi <cbs412@naver.com>
2022-02-15 10:53:19 +0900

    Modifires for list, for forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,23 +8,36 @@ const Home: NextPage = () => {
     <div className="grid min-h-screen gap-10 bg-slate-400 py-20 px-20">
       <div className=" rounded-3xl bg-white p-6 shadow-2xl">
         <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 mb-3 flex justify-between">
-          <span className="text-gray-500">Gry Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Gry Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
+        <ul>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div
+              key={i}
+              className="my-2 mb-3 flex justify-between odd:bg-blue-50 even:bg-yellow-500"
+            >
+              <span className="text-gray-500">Gry Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((c, i) => (
+            <li className="bg-red-500 py-2 empty:hidden" key={i}>
+              {c}
+            </li>
+          ))}
+        </ul>
         <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mx-auto mt-5 w-1/2 rounded-xl bg-blue-500 p-3 text-center text-white">
+        <button
+          className="mx-auto mt-5 block w-3/4 rounded-xl bg-blue-500 p-3 text-center 
+        text-white hover:bg-teal-500 hover:text-black focus:bg-red-500 active:bg-yellow-500"
+        >
           checkout
-        </div>
+        </button>
       </div>
-      <div className="overflow-hidden rounded-3xl bg-white shadow-2xl">
+      <div className="group overflow-hidden rounded-3xl bg-white shadow-2xl">
         <div className="bg-blue-500 p-6 pb-14">
           <span className="text-2xl text-white">Profile</span>
         </div>
@@ -34,7 +47,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Order</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="h-24 w-24 rounded-full bg-red-400 transition-colors group-hover:bg-blue-50" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
@@ -59,10 +72,10 @@ const Home: NextPage = () => {
           <span className="text-xl font-medium">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex items-center justify-between">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className="space-x-2">
+              <button className="bg-opacity-2 h-5 w-5 rounded-full bg-yellow-500 ring-yellow-500 ring-offset-2 transition focus:ring-2"></button>
+              <button className="bg-opacity-2 h-5 w-5 rounded-full bg-indigo-500 ring-indigo-500 ring-offset-2 transition focus:ring-2"></button>
+              <button className="bg-opacity-2 h-5 w-5 rounded-full bg-teal-500 ring-teal-500 ring-offset-2 transition focus:ring-2"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className="flex aspect-square w-10 items-center justify-center rounded-lg bg-blue-200 p-1.5 text-xl font-medium text-gray-500">
@@ -82,6 +95,31 @@ const Home: NextPage = () => {
           </button>
         </div>
       </div>
+      <form className="flex flex-col space-y-2 bg-white p-5">
+        {/*  <input
+          type="text"
+          required
+          placeholder="Username"
+          className="border-yellow-500 placeholder:text-red-500 placeholder-shown:bg-teal-500 required:border-2
+        required:bg-yellow-500 valid:bg-teal-500 invalid:bg-red-500 disabled:opacity-0"
+        />  */}
+        <input
+          type="text"
+          required
+          placeholder="username"
+          className="brder-gray-400 peer rounded-md border p-1"
+        />
+        <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+          This input is invalid
+        </span>
+        <span className="hidden peer-valid:block peer-valid:text-green-500">
+          This input is valid
+        </span>
+        <span className="hidden peer-valid:text-amber-500 peer-hover:block">
+          This input is valid
+        </span>
+        <input type="submit" value="Login" className="bg-white" />
+      </form>
     </div>
   );
 };
