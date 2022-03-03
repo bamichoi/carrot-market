4333fc729eb9d7fee7c016296c4a186d3eb600fd
beomseok.choi <cbs412@naver.com>
2022-03-03 21:27:04 +0900

    prisma authentication

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,16 +5,6 @@ import client from "@libs/server/client";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-  });
   const token = await client.token.create({
     data: {
       payload: "1234",
