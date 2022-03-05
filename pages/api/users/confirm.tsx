02ad6aba482d5e88556a8159f1943ddc99d7c87f
beomseok.choi <cbs412@naver.com>
2022-03-05 20:23:25 +0900

    Token UI, Serverless session using iron-session

---
+++ pages/api/users/confirm.tsx
@@ -0,0 +1,27 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { token } = req.body;
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+  res.status(200).end();
+}
+
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "21480235892673896128369458123643208512649065812369",
+});
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,28 +1,55 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
-      payload: "1234",
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
-  console.log(token);
-  res.status(200).end();
+  if (phone) {
+    /* const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message); */
+  } else if (email) {
+    /*  const email = await mail.send({
+      from: "cbs412@naver.com",
+      to: "cbs412@naver.com",
+      subject: "Your Carrot Market Verification Eamil",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email); */
+  }
+
+  res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
