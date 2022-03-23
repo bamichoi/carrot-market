8e38fa39b28cedf84bc6ce639b457cfdd3c52cb7
beomseok.choi <cbs412@naver.com>
2022-03-23 20:16:14 +0900

    purchases, sales, favs api and profile page

---
+++ pages/api/users/me/favs.ts
@@ -0,0 +1,27 @@
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+  const favs = await client.fav.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  res.json({
+    ok: true,
+    favs,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
