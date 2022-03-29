ce57ce562471404b8e46c0ce9506018a8bb00a2e
beomseok.choi <cbs412@naver.com>
2022-03-29 22:00:33 +0900

    streams

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -1,18 +1,26 @@
+import { Stream } from "@prisma/client";
 import type { NextPage } from "next";
 import Link from "next/link";
+import useSWR from "swr";
 import FloatingButton from "../../components/floating-button";
 import Layout from "../../components/layout";
 
-const Live: NextPage = () => {
+interface StreamsResponse {
+  ok: boolean;
+  streams: Stream[];
+}
+
+const Stream: NextPage = () => {
+  const { data } = useSWR<StreamsResponse>("/api/steams");
   return (
     <Layout hasTabBar title="라이브">
       <div className=" space-y-4 divide-y-[1px]">
-        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Link key={i} href={`/live/${i}`}>
+        {data?.streams.map((stream) => (
+          <Link key={stream.id} href={`/streams/${stream.id}`}>
             <a className="block px-4  pt-4">
               <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
               <h1 className="mt-2 text-2xl font-bold text-gray-900">
-                Galaxy S50
+                {stream.name}
               </h1>
             </a>
           </Link>
@@ -37,4 +45,4 @@ const Live: NextPage = () => {
     </Layout>
   );
 };
-export default Live;
+export default Stream;
