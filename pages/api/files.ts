6436fa9b3156f1cfac4c313868ff7191c9f72545
beomseok.choi <cbs412@naver.com>
2022-04-04 20:56:33 +0900

    cloudeflare image direct upload

---
+++ pages/api/files.ts
@@ -0,0 +1,28 @@
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const response = await (
+    await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_IMAGE_ID}/images/v1/direct_upload`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CF_IMAGE_TOKEN}`,
+        },
+      }
+    )
+  ).json();
+  res.json({
+    ok: true,
+    ...response.result,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
